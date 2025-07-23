2b7cad9051932fcfcf0b52e1508533e8c4dc09a9
Julien Newman <juliennewman@juliens-air.home>
2025-07-23 11:45:45 -0400

    adjusted text of ceo letter

--- src/components/CEOLetter.tsx
+++ src/components/CEOLetter.tsx
@@ -29,12 +29,12 @@ export function CEOLetter() {
               <p>
                 You&apos;re the best at your job. You have an amazing team who
                 supports you. And you may even have a world class EA. But the
-                truth is that you need more support than any growing company can
-                give
+                truth is that you need more support than a growing company can
+                give you
               </p>
 
               <p>
-                With hndl you{" "}
+                With hndl you can now{" "}
                 <strong>
                   get more hours in your day easily, quickly, reliably
                 </strong>
@@ -46,24 +46,28 @@ export function CEOLetter() {
                 gathering internal data
               </p>
 
-              <p>Our 3 specialized AIs do these 3 tasks for you</p>
-
               <p>
-                Each of these 3 AIs are built to do 1 specific task for C-Suite
-                Execs in SaaS businesses. And our 3 AIs share what they learn so
-                you don&apos;t need to teach the same thing over and over to
-                different AIs. This means that we handle your tasks fast and in
-                a way that&apos;s always high quality
+                Most of the work gets done by 3 specialized AIs who each do one
+                of the 3 tasks. Then a trained human who you pick takes care of
+                the last mile
               </p>
 
-              <p>Then a human takes care of the last mile</p>
+              <p>
+                One example of a popular way other C-Suite Execs use hndl is to
+                triage emails to highlight emails where there&apos;s an
+                opportunity to celebrate your team&apos;s success
+              </p>
 
               <p>
-                You easily pick 1 named human for each task. This humans handles
-                the parts which AI can&apos;t do well. And the human element is
-                so integrated that you don&apos;t notice that humans are part of
-                the process. Having a human do the last mile means your tasks
-                get handled in a way that&apos;s actually good
+                Another example is that Execs show us how to create decks in the
+                style their CEO prefers then they get hndl to create decks for
+                meetings where their CEO is participating
+              </p>
+
+              <p>
+                Or Execs will get hndl to listen in on sales calls and customer
+                service calls then report back with counter-intuitive
+                perspectives from customers
               </p>
 
               <p>
