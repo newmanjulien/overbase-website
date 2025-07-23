4d08bb489602c8a69dfa2c878289080dac0d16d9
Julien Newman <juliennewman@juliens-air.home>
2025-07-22 20:38:00 -0400

    cleaned up layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "hndl",
   description: "Task handler for C-Suite Execs",
+  icons: "/favicon.ico",
 };
 
 export default function RootLayout({
