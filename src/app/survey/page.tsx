e3d6c3dc04b25afb3c1168c2ee857092f66d8aa8
Julien Newman <juliennewman@juliens-air.home>
2025-08-25 10:19:03 -0400

    fixing survey

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
-import posthog from "posthog-js";
+import posthog, { Survey } from "posthog-js";
 
 export default function SurveyPage() {
   useEffect(() => {
+    // Initialize PostHog
     posthog.init("phc_azK72AdLBTsfE9LVWyENBcgi5DMHARdjtTHU2XYdj5h", {
       api_host: "https://us.posthog.com",
       person_profiles: "identified_only",
+    });
+
+    // Fetch surveys and render them
+    posthog.getSurveys((surveys: Survey[]) => {
+      surveys.forEach((survey) => {
+        // Render each survey into the body
+        posthog.renderSurvey(survey.id, "body");
+      });
     });
   }, []);
 
