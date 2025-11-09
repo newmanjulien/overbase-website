9a19445634d4315fbd690642fd000d012019bd38
Julien Newman <juliennewman@Juliens-MacBook-Air.local>
2025-11-09 13:35:25 -0500

    adjusted CEO letter words

--- src/app/CEOLetter.tsx
+++ src/app/CEOLetter.tsx
@@ -23,58 +23,62 @@ export function CEOLetter({ onJoinWaitlist }: { onJoinWaitlist: () => void }) {
 
             <div className="space-y-6 text-gray-600 text-base leading-relaxed">
               <p>
-                Being a world class Customer Success Manager means knowing
-                what's going on with your customers better than everyone else.
-                But boy is that hard to do when customer data isn't easy to get
+                ELT, ETL or CDP? Reverse ETL? Data lake? Data warehouse? What
+                are these things and why can't you just get the data you need?
               </p>
 
               <p>
-                The data you need to prepare for your next QBR, to understand
-                the health of an account, to make the case for an expansion.
-                That data is all over the place, hard to access and often
-                outdated
+                Our businesses have way too much customer data which has made it
+                remarkably hard - and often just impossible - to know what's
+                going on with our customers
               </p>
 
               <p>
-                You might be constantly querying Posthog to get usage data.
-                Checking Pylon and Linear for open tickets. Looking in Gainsight
-                to understand the account history. Potentially even begging the
-                dev team to help you get some more niche data. Then you might
-                need to also double check much of that data because it could be
-                outdated
+                But adding many more layers of complication is clearly not the
+                way to reduce complication. And creating yet another database
+                can't possibly make things better. Even if that extra database
+                is marketed as a "single source of truth"
               </p>
 
               <p>
                 <strong className="font-medium">
-                  Why is it so hard to get the data you need?
+                  There's got to be an easy way of getting customer data
                 </strong>
               </p>
 
-              <p>With Overbase, CSMs can now easily get any customer data</p>
-
               <p>
-                You simply explain what data you need in plain English. Then an
-                AI agent quickly and automatically gathers the data in a way
-                that's both complete and accurate
+                <strong className="font-medium">
+                  Overbase is the first data system that uses AI agents to fetch
+                  customer data.
+                </strong>{" "}
+                You simply explain what you need in plain English. Then an agent
+                quickly and automatically gathers the data in a way that's
+                reliable, secure and complete
               </p>
 
               <p>
-                This AI agent can gather data from all of your platforms and
-                tools. It can get structured data from Posthog, Pylon,
-                Gainsight, etc. But also unstructured data from Gong, Slack,
-                Gmail, Notion, etc
+                AI agents can easily gather data from all of your platforms and
+                tools with close to 0 setup. Agents easily get structured data
+                from Posthog, Zendesk, Salesforce, etc. But also unstructured
+                data from Gong, Slack, Gmail, Notion, etc
               </p>
 
               <p>
                 And to guarantee the quality of the data, one of Overbase's
                 human data scientists always reviews the data which the AI agent
-                gathered for you
+                gathered for you to be sure the AI did it properly
               </p>
 
               <p>
                 The result is that you go to 1 single place to get any customer
                 data. You get that data with close to 0 effort. And the data is
                 always accurate and complete
+              </p>
+
+              <p>
+                And to top it off, this agentic architecture is more secure and
+                compliant than all existing systems because we don't store any
+                of your data
               </p>
 
               <p>
