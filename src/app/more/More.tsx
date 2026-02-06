e440b09eed094bc6666e64287ce34bb8be9250d5
Julien Newman <juliennewman@juliens-air.home>
2026-02-06 18:27:35 -0500

    cleaned up layout components

--- src/app/more/More.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { BlogPageLayout } from "@/components/layouts/BlogPageLayout";
-import { moreHero, moreSections } from "@/app/more/more-content";
-
-export function More() {
-  return <BlogPageLayout hero={moreHero} sections={moreSections} />;
-}
