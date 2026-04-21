4125c8607e367ee6979a2af30ec0f398c97f37e9
Julien Newman <juliennewman@juliens-air.home>
2026-04-21 10:57:58 -0400

    adjusted mission in career

--- src/app/(site)/careers/careers.tsx
+++ src/app/(site)/careers/careers.tsx
@@ -9,7 +9,7 @@ export default function Careers() {
       <Hero
         title={
           <>
-            Help us build the future of <span className="italic">data</span>
+            Build the future of <span className="italic">software</span>
           </>
         }
         cta={{ label: "See roles", targetId: "careers-table" }}
@@ -21,30 +21,24 @@ export default function Careers() {
         <div className="grid md:grid-cols-2 gap-14 md:gap-30">
           <div>
             <h2 className="text-2xl text-gray-900 font-medium leading-tight">
-              Be part of leapfrogging legacy data systems
+              Be part of turning all business software invisible
             </h2>
           </div>
           <div className="space-y-6 text-gray-500 text-[15px]">
             <p>
-              For decades, data centralizers promised insight but delivered
-              frustration, slow processes, and costly complexity
+              For decades, we've been adding more and more confusing interfaces
+              to our work with mixed results
             </p>
             <p>
-              Overbase gets reliable answers from even the most disconnected
-              data. We aren’t just patching retro ‘90s tech with LLMs, we're
-              reimagining data management from first principles
+              Overbase makes the promise of business software real by turning it
+              invisible and just delivering the value in a seamless and
+              automatic manner
             </p>
 
             <p>
-              Together we'll start by leapfrogging CRMs. Then we'll replace all
-              legacy data systems with a modern decentralized alternative (
-              <Link
-                href={getBlogHref("decentralized-data")}
-                className="text-[#4F769E] underline underline-offset-2 decoration-[#4F769E]/65"
-              >
-                link
-              </Link>
-              )
+              Together we'll start by turning sales software invisible. Then
+              we'll replace all legacy vendors by turning the entire business
+              software stack into an invisible layer
             </p>
           </div>
         </div>
